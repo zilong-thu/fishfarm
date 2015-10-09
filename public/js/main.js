d2410fbcd417a73dfb4048022aff9377138d0cd5
zilong-thu <809052335@qq.com>
2015-10-09 22:32:48 +0800

    add new video players

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,9 @@ $(function(){
         },
         video: {
             src: 'http://7xn4yl.com1.z0.glb.clouddn.com/vid_20150804_112223.m4v'
+        },
+        poster: {
+            src: ''
         }
     }, {
         title: '视频2：进食中的观赏鱼',
@@ -22,15 +25,50 @@ $(function(){
         },
         video: {
             src: 'http://7xn4yl.com1.z0.glb.clouddn.com/vid_20150928_105055.m4v'
+        },
+        poster: {
+            src: ''
         }
     }];
 
     util.renderView({videoList: videoList}, '#tpl-video-list', '#video-list-container');
 
-    $('#video-list-container').delegate('.item-pre', 'click', function(event){
+
+    /** 
+     * 播放器组件, 右侧
+    **/
+    var domObj = {};
+    domObj.video = $('#main-video-player')[0];
+    domObj.isPlaying = false;
+
+    var _videoListContainer = $('#video-list-container');
+    _videoListContainer.delegate('.item-pre', 'click', function(event){
         var _self = $(this);
         var src = _self.data('videosrc');
 
-        $('#main-video-player').attr('src', src);
+        _videoListContainer.find('.item-pre.active').removeClass('active');
+        _self.addClass('active');
+
+        $(domObj.video).attr('src', src);
+        domObj.video.play();
+        domObj.isPlaying = true;
     });
+
+
+    /** 
+     * 播放器组件, 视频本身
+    **/
+    $('#main-video-player').click(function(event){
+        var _self = $(this);
+
+        if (domObj.isPlaying) {
+            domObj.video.pause();
+            domObj.isPlaying = false;
+        }else{
+            domObj.video.play();
+            domObj.isPlaying = true;
+        }
+        
+    });
+
 });
